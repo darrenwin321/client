46dfd8c2d1cebbea5e4e36d8e2a3986ca0e1e052
darrenwin321 <89400271+darrenwin321@users.noreply.github.com>
2023-11-01 23:41:17 -0400

    Update AppBanner.js

--- src/components/AppBanner.js
+++ src/components/AppBanner.js
@@ -5,7 +5,7 @@ import React from 'react'
 export default function AppBanner() {
     return(
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-          <a className="navbar-brand">Navbar</a>
+          <a className="navbar-brand" href="/#">Navbar</a>
           <button type="button" className="btn btn-primary">Click Here (Test)</button>
         </nav>
     )
